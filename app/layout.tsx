30e8de262783a1de8d39244c059fd22dd40d6092
cryskram <vageeshgn2005@gmail.com>
2025-07-23 02:22:45 +0530

    next auth setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Space_Grotesk } from "next/font/google";
 import "./globals.css";
+import { Providers } from "@/components/Providers";
 
 const font = Space_Grotesk({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${font.className} antialiased`}>{children}</body>
+      <body className={`${font.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
